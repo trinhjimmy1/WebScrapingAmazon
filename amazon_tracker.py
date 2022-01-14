f8c91eb654b894efbeef2d79b7788d773d0c1c9e
jim <trinh.jimmy10@gmail.com>
2022-01-14 18:57:49 +0100

    first commit

---
+++ amazon_tracker.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from main import (
+  get_chrome_options,
+  get_chrome_web_driver,
+  NAME,
+  CURRENCY,
+  FILTERS,
+  BASE_URL,
+  DIRECTORY
+)
+
+from selenium import webdriver
+
+# Dans un premier temps je vais mettre en place une classe qui me permettra de recueillir la data voulu
+class AmazonData:
+  pass
